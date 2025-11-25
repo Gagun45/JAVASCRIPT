b7f2f47e5608665847f36fe11f7a7f172452957a
Gagun45 <selyanchyn45@gmail.com>
2025-11-26 01:31:19 +0200

    lesson1 hw done

---
+++ homework/hw1/main.js
@@ -0,0 +1,59 @@
+{
+// dYQNrBV
+    const a = 'hello'
+    const b = 'owu'
+    const c = 'com'
+    const d = 'ua'
+    const f = 1
+    const e = 10
+    const z = -999
+    const x = 123
+    const v = 3.14
+    const n = 2.7
+    const t = 16
+    const q = true
+    const w = false
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(f)
+    console.log(e)
+    console.log(z)
+    console.log(x)
+    console.log(v)
+    console.log(n)
+    console.log(t)
+    console.log(q)
+    console.log(w)
+}
+
+{
+// 6Qb97gsv
+    const firstName = 'Oleksandr'
+    const middleName = 'Mykolayovych'
+    const lastName = 'Selianchyn'
+    const person = firstName + ' ' + middleName + ' ' + lastName
+    console.log(person)
+}
+
+{
+// 4N0y5tufA
+    let a = 100
+    let b = '100'
+    let c = true
+
+    console.log(typeof a)
+    console.log(typeof b)
+    console.log(typeof c)
+}
+
+{
+// ruUtWDUI
+    const enter = 'Enter your'
+    const firstName = window.prompt(`${enter} first name: `)
+    const middleName = window.prompt(`${enter} last name: `)
+    const age = window.prompt(`${enter} age: `)
+    console.log(firstName, middleName, age)
+}
