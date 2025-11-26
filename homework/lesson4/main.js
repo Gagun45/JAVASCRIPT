27e88354cc240d8a74fc2af3a037678dd8b68c86
Gagun45 <selyanchyn45@gmail.com>
2025-11-27 01:37:33 +0200

    lesson4 hw edited

--- homework/lesson4/main.js
+++ homework/lesson4/main.js
@@ -1,19 +1,19 @@
 function exchange(sumUAH, currencyValues, exchangeCurrency) {
     let rate;
-    let found = false
     let i = 0
-    while (i < currencyValues.length && !found) {
+    while (i < currencyValues.length) {
         const curr = currencyValues[i]
         if (curr.currency === exchangeCurrency) {
             rate = curr.value
-            found = true
             break;
         }
         i++
     }
-    if (!found) return 'Wrong currency input'
+    if (rate===undefined) return 'Wrong currency input'
 
     return (sumUAH / rate).toFixed(2)
 }
 
-console.log(exchange(10000, [{currency: 'USD', value: 15}, {currency: 'EUR', value: 42}], 'EUR'));
+console.log(exchange(10000, [{currency: 'USD', value: 20}, {currency: 'EUR', value: 10}], 'EUR'));
+console.log(exchange(10000, [{currency: 'USD', value: 20}, {currency: 'EUR', value: 10}], 'USD'));
+console.log(exchange(10000, [{currency: 'USD', value: 20}, {currency: 'EUR', value: 10}], 'asd'));
